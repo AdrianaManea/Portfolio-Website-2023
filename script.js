8e9fa8ef6abd7129d2656269104e359821132287
Adriana Manea <adrianna.manea@gmail.com>
2023-03-06 18:56:11 +0100

    add toggle light/dark mode btn

--- script.js
+++ script.js
@@ -41,6 +41,29 @@ function PageTransitions() {
 			element.classList.add('active');
 		}
 	})
+
+	// Theme btn
+	const themeBtn = document.querySelector('.theme-btn');
+	themeBtn.addEventListener('click', () => {
+		let elem = document.body;
+		elem.classList.toggle('light-mode');
+	});
 }
 
 PageTransitions()
+
+
+
+// (function () {
+// 	[...document.querySelectorAll(".control")].forEach(button => {
+// 			button.addEventListener("click", function() {
+// 					document.querySelector(".active-btn").classList.remove("active-btn");
+// 					this.classList.add("active-btn");
+// 					document.querySelector(".active").classList.remove("active");
+// 					document.getElementById(button.dataset.id).classList.add("active");
+// 			})
+// 	});
+// 	document.querySelector(".theme-btn").addEventListener("click", () => {
+// 			document.body.classList.toggle("light-mode");
+// 	})
+// })();
